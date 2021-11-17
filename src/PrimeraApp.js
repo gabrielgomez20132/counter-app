1621189da197dee9aa4e4c8eb7180560b9827d43
gabrielgomez20132 <gabrielgomez_100@hotmail.com>
2021-11-17 10:32:13 -0300

    1er commit counter app

---
+++ src/PrimeraApp.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import React, { Fragment } from 'react';
+
+//funcional components 
+const PrimeraApp = ( props ) => {
+
+    console.log(props);
+    return (
+        <>   
+            <h1>HOLA MUNDO</h1>
+            <h2>AL MUNDO</h2>
+        </>
+    );
+}
+export default PrimeraApp;
